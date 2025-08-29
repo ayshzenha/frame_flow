afc9c251425915879d45c31c2d2b2c9f8a90e839
aysha zenha <179464573+ayshzenha@users.noreply.github.com>
2025-08-29 18:46:24 +0530

    new brand added

--- lib/modules/home/category_card.dart
+++ lib/modules/home/category_card.dart
@@ -1,18 +1,57 @@
+import 'dart:ui'; // for ImageFilter
 import 'package:flutter/material.dart';
 
 class CategoryCard extends StatelessWidget {
   final String? imagePath;
-  const CategoryCard({this.imagePath});
+  final String? label;
+
+  const CategoryCard({this.imagePath, this.label});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
+    return SizedBox(
       width: MediaQuery.of(context).size.width * 0.4,
-
       child: ClipRRect(
         borderRadius: BorderRadius.circular(16),
-        child: Image.asset(imagePath ?? '', fit: BoxFit.cover),
+        child: Stack(
+          children: [
+            // Background Image (not blurred)
+            Image.asset(
+              imagePath ?? '',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+
+            // Glassmorphism container only for label
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.all(8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 6,
+                      ),
+                      color: Colors.white.withOpacity(0.2), // frosted glass
+                      child: Text(
+                        label ?? '',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/modules/home/home_widgets.dart
+++ lib/modules/home/home_widgets.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frame_flow/modules/home/category_card.dart';
+import 'package:frame_flow/modules/home/feature_product_card.dart';
 
 import 'package:frame_flow/utils/image_utils.dart';
 
@@ -10,62 +11,108 @@ class HomeWidgets {
     Imageutil.electronics,
     Imageutil.shoes,
   ];
+
+  List<String> categoryName = [
+    "Accessories",
+    "Clothing",
+    "electronics",
+    "Shoes",
+  ];
+  List<String> items = [
+    Imageutil.accessories,
+    Imageutil.clothing,
+    Imageutil.electronics,
+    Imageutil.shoes,
+  ];
   Widget homeBodyWidget() {
-    return ListView(
-      scrollDirection: Axis.vertical,
+    return SingleChildScrollView(
       padding: EdgeInsets.all(15.0),
-      children: [
-        Container(
-          height: 40,
-          width: 20,
-          padding: EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: TextField(
-            decoration: InputDecoration(
-              icon: Icon(Icons.search_rounded),
-              hintText: 'Search products',
-              border: InputBorder.none,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 40,
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.search_rounded),
+                hintText: 'Search products',
+                border: InputBorder.none,
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 25),
-        Container(
-          height: 200,
-          width: 50,
 
-          child: ClipRRect(
+          SizedBox(height: 25),
+
+          ClipRRect(
             borderRadius: BorderRadius.circular(16),
-            child: Image.asset(Imageutil.shopping, fit: BoxFit.cover),
+            child: Image.asset(
+              Imageutil.shopping,
+              fit: BoxFit.cover,
+              height: 200,
+              width: 400,
+            ),
           ),
-        ),
 
-        SizedBox(height: 10),
-        Text(
-          "Browse categories ",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 30,
-            fontFamily: "Libertinus Serif Display",
+          SizedBox(height: 10),
+
+          Text(
+            "Browse categories",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontFamily: "Libertinus Serif Display",
+            ),
           ),
-        ),
-        GridView.builder(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
+          GridView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemCount: catogaries.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CategoryCard(
+                  imagePath: catogaries[index],
+                  label: categoryName[index],
+                ),
+              );
+            },
           ),
-          itemCount: catogaries.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CategoryCard(imagePath: catogaries[index]),
-            );
-          },
-        ),
-      ],
+
+          SizedBox(height: 20),
+
+          Center(
+            child: Text(
+              "Featured Brands".toUpperCase(),
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+
+          SizedBox(
+            height: 220,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: items.length,
+              itemBuilder: (context, index1) {
+                return Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: FeatureProductCard(
+                    brandname: items[index1],
+                    imagePath: items[index1],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
